040d1ff9752fc180c5d9718eec3cc0017072da0f
yosef moshe <yosef moshe>
2018-03-09 11:14:14 +0200

    added heroku to allowed hosts

--- efi/settings.py
+++ efi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bjotijs+h#(ew1#+r!+1wj1g11bi3_*zuxfqg)a-h=p46z148+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['caneti.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
